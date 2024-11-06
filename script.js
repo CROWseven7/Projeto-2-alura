f0ce5de9771bd70fa31b6aacfd2a0a132e6855ce
CROWseven7 <mairamillerdonega@gmail.com>
2024-11-06 09:50:32 -0300

    update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const avanca = document.querySelectorALL('.btn-proximo');
 
 avanca.forEach(button => {
   button.addEventListener('click',function(){
-    const atual= document.query
+    const atual= document.querySelector('.ativo');
+    const proximoPasso = 'passo-' + tthis.getAttribute('data-proximo');
+    
+    actual.classList.remove('ativo');
+    document.getElementById(proximoPasso).classList.add('ativo');
   })
 })
